13e310e55bc89a9f7f296e0336ee93ed6c435397
mark gras ferrer <user.name>
2025-05-19 09:12:29 +0200

    Modificat BrawlersWrapper i brawlers

--- Pr2/src/model/Brawler.java
+++ Pr2/src/model/Brawler.java
@@ -7,6 +7,9 @@ public class Brawler {
     private int id;
     private String name;
 
+    /*Com el json que ens passa la api te diferents items dins de altres items necessitem clases dintre de aquesta
+    * per poder gestionar tot. (Rarity esta dintre del brawler). starpower i gadget es una List ja que cada brawler
+    * te 2 de cada.*/
     @SerializedName("class")
     private BrawlerClass brawlerClass;
 
@@ -15,8 +18,6 @@ public class Brawler {
     private List<StarPower> starPowers;
     private List<Gadget> gadgets;
 
-    // Getters y setters
-
     public int getId() { return id; }
     public String getName() { return name; }
     public BrawlerClass getBrawlerClass() { return brawlerClass; }
@@ -24,8 +25,8 @@ public class Brawler {
     public List<StarPower> getStarPowers() { return starPowers; }
     public List<Gadget> getGadgets() { return gadgets; }
 
-    // Clases internas para las estructuras anidadas
 
+    /*Clases de les que he parlat abans per poder gestionar els objectes que hi ha dintre del brawler*/
     public static class BrawlerClass {
         private int id;
         private String name;
