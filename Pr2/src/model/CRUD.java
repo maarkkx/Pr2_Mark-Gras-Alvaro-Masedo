beec23e10915877e8e79ab20905f3d428ac9883f
Álvaro Masedo <alvaromasedoperez@gmail.com>
2025-05-20 10:50:30 +0200

    CRUD

--- Pr2/src/model/CRUD.java
+++ Pr2/src/model/CRUD.java
@@ -1,4 +1,13 @@
 package model;
 
-public interface CRUD {
+public interface CRUD<Brawler>{
+    void crear(Brawler obj);
+
+    void actualitzar(Brawler obj);
+
+    void eliminar(int id);
+
+    void llegirUnaEntrada();
+
+    void llegirTot();
 }
