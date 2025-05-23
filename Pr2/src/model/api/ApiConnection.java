0ce7bc70b25e249d844ccec1ad80e56e6928f3ec
mark gras ferrer <user.name>
2025-05-23 10:41:58 +0200

    Actualitzar només un brawler

--- Pr2/src/model/api/ApiConnection.java
+++ Pr2/src/model/api/ApiConnection.java
@@ -81,6 +81,37 @@ public class ApiConnection {
         }
     }
 
+    public static void llegirJsonUnBrawler() {
+        Gson gson = new Gson();
+        try (FileReader reader = new FileReader("src/jsons/unBrawler.json")) {
+
+            RootBrawlers root = gson.fromJson(reader, RootBrawlers.class);
+            List<Brawler> brawlers = root.getList();
+
+            for (Brawler b : brawlers) {
+                System.out.println("Brawler: " + b.getName());
+                System.out.println("  Class: " + b.getBrawlerClass().getName());
+                System.out.println("  Rarity: " + b.getRarity().getName());
+
+                if (b.getStarPowers() != null) {
+                    for (Brawler.StarPower sp : b.getStarPowers()) {
+                        System.out.println("  Star Power: " + sp.getName() + " - " + sp.getDescription());
+                    }
+                }
+
+                if (b.getGadgets() != null) {
+                    for (Brawler.Gadget gd : b.getGadgets()) {
+                        System.out.println("  Gadget: " + gd.getName() + " - " + gd.getDescription());
+                    }
+                }
+                System.out.println();
+            }
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
     public static void afegirBrawlers() {
         Gson gson = new Gson();
         try (FileReader reader = new FileReader("src/jsons/nous_brawlers.json")) {
@@ -109,6 +140,37 @@ public class ApiConnection {
     public static void actualitzarBrawlers() {
         Gson gson = new Gson();
         try (FileReader reader = new FileReader("src/jsons/nous_brawlers.json")) {
+
+            RootBrawlers root = gson.fromJson(reader, RootBrawlers.class);
+            List<Brawler> brawlers = root.getList();
+
+            for (Brawler b : brawlers) {
+                SQLiteRarityDAO daoR = new SQLiteRarityDAO();
+                daoR.actualitzar(b);
+                SQLiteClassDAO daoC = new SQLiteClassDAO();
+                daoC.actualitzar(b);
+                SQLiteBrawlerDAO dao = new SQLiteBrawlerDAO();
+                dao.actualitzar(b);
+                SQLiteStarpowerDAO daoSP = new SQLiteStarpowerDAO();
+                daoSP.actualitzar(b);
+                SQLiteGadgetDAO daoG = new SQLiteGadgetDAO();
+                daoG.actualitzar(b);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void actualitzarUnBrawler() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Escriu l'id del brawler que vols modificar: ");
+        String id = scan.nextLine();
+        String url = "https://api.brawlify.com/v1/brawlers/" + id;
+        getJson(url, "src/jsons/unBrawler.json");
+        llegirJsonUnBrawler();
+
+        Gson gson = new Gson();
+        try (FileReader reader = new FileReader("src/jsons/unBrawler.json")) {
 
             RootBrawlers root = gson.fromJson(reader, RootBrawlers.class);
             List<Brawler> brawlers = root.getList();
