7c3abcfdf39cdf7b2850fa340ccadca1255b7fd4
mark gras ferrer <user.name>
2025-05-23 10:02:34 +0200

    Llegir una entrada y totes starpowers

--- Pr2/src/model/dao/SQLite/SQLiteStarpowerDAO.java
+++ Pr2/src/model/dao/SQLite/SQLiteStarpowerDAO.java
@@ -99,34 +99,66 @@ public class SQLiteStarpowerDAO implements CRUD {
     @Override
     public void llegirUnaEntrada() {
         Scanner scan = new Scanner(System.in);
-        Connection con = DBConnection.openCon();
-        try {
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM starpowers WHERE bralwer_id = ?");
-            System.out.println("Escriu l'id del brawler que vols veure les starpower");
-            int id = scan.nextInt();
-            scan.nextLine();
-            ps.setInt(1, id);
-            ps.executeUpdate();
 
-            ps.close();
+        // Query SQL per llegir un brawler
+        String sql = "SELECT * FROM starpowers WHERE brawler_id = ?";
+
+        // Obre la connexió a la BD
+        Connection con = DBConnection.openCon();
+
+        try (PreparedStatement stmt = con.prepareStatement(sql)){
+            // Demana el id del Brawler a consultar
+            System.out.println("Escriu el ID del Brawler que vols comprovar: ");
+            int id = scan.nextInt();
+
+            // Comprova si el brawler existeix
+            PreparedStatement check = con.prepareStatement("SELECT COUNT(*) FROM brawlers WHERE brawler_id = ?");
+            check.setInt(1,id);
+            ResultSet rs = check.executeQuery();
+            stmt.setInt(1,id);
+            ResultSet rs2 = stmt.executeQuery();
+            while(rs2.next() && rs.getInt(1) == 1){
+                // Mostra les dades del gadget
+                System.out.printf("StarPower ID: %-10d Nom: %-25s Descripcio: %-130s Brawler ID: %-5d\n",
+                        rs2.getInt("starpower_id"),
+                        rs2.getString("nom"),
+                        rs2.getString("descripcio"),
+                        rs2.getInt("brawler_id")
+                );
+            }
+
+            check.close();
+            stmt.close();
             con.close();
-            scan.close();
         } catch (SQLException e) {
-            System.out.println(e);
+            System.out.println("Error al llegir dades de la base de dades");
         }
     }
 
     @Override
     public void llegirTot() {
+        // Query SQL per llegir tots els gadgets
+        String sql = "SELECT * FROM starpowers";
+
+        // Obre la connexió a la BD
         Connection con = DBConnection.openCon();
-        try {
-            PreparedStatement ps = con.prepareStatement("SELECT * FROM starpowers");
-            System.out.println("Escriu l'id del brawler que vols veure les starpower");
-            ps.executeUpdate();
-            ps.close();
+
+        try(PreparedStatement stmt = con.prepareStatement(sql)){
+            // Executa la consulta i processa els resultats
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()){
+                System.out.printf("StarPower ID: %-10d Nom: %-25s Descripcio: %-130s Brawler ID: %-5d\n",
+                        rs.getInt("starpower_id"),
+                        rs.getString("nom"),
+                        rs.getString("descripcio"),
+                        rs.getInt("brawler_id")
+                );
+            }
+            stmt.close();
             con.close();
-        } catch (SQLException e) {
-            System.out.println(e);
+        } catch (SQLException e){
+            System.out.println("Error al llegir dades de la Base de Dades");
         }
     }
 }
