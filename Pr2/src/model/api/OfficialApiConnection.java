d0a04c2980e9f32c842ce35054c4cf1c18cb6a70
Álvaro Masedo <alvaromasedoperez@gmail.com>
2025-05-23 10:45:59 +0200

    Api oficial

--- Pr2/src/model/api/OfficialApiConnection.java
+++ Pr2/src/model/api/OfficialApiConnection.java
@@ -1,41 +1,42 @@
 package model.api;
 
 import com.google.gson.Gson;
-import model.BrawlersWrapper;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import model.main;
 
 
+import java.io.File;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class OfficialApiConnection {
-    public static String api_url = "https://api.brawlstars.com/v1/brawlers";
-    public static String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjkxNzU0YTE3LTZkYzAtNDU4MC04MWQ0LTVlNDg0ODY2OGFiNCIsImlhdCI6MTc0NzY0ODIyMiwic3ViIjoiZGV2ZWxvcGVyLzk1YzQzMzgzLTMzNmItZDAzMS1lZWNlLTg2NGNjOGYwNzI2YiIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiODUuNTAuMTYwLjQzIl0sInR5cGUiOiJjbGllbnQifV19.YAwNvmseFl9dK5zeiol4iwM_rYAmCamcxmWAd9dXSSo5Kye2yhvHoB4BpUkv6aB1ShX2kR89g0ijO63A-h9lIw";
-
-    public static BrawlersWrapper fetchBrawlers(){
-        Gson gson = new Gson();
-
+    public static void getJson(String endpoint, String ruta){
         try {
-            URL url = new URL(api_url);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            //Creacio conexio HTTP amb la api
+            URL url = new URL(endpoint);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
 
-            connection.setRequestMethod("GET");
-
-            connection.setRequestProperty("Authorization", token);
-
-            if(connection.getResponseCode() == 200){
-                InputStreamReader reader = new InputStreamReader(connection.getInputStream());
-                BrawlersWrapper wrapper = gson.fromJson(reader, BrawlersWrapper.class);
-                reader.close();
-                return wrapper;
-            } else {
-                System.out.println("Error: código " + connection.getResponseCode());
+            //Llegir el json rebut i donar-li format
+            Scanner scan = new Scanner(con.getInputStream());
+            StringBuilder jsonSenseFormat = new StringBuilder();
+            while (scan.hasNext()){
+                jsonSenseFormat.append(scan.nextLine();
             }
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonElement jsonElement = JsonParser.parseString(jsonSenseFormat.toString());
+            String jsonFormatejat = gson.toJson(jsonElement);
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            //Crear arxiu i guardar el contingut
+            File file = new File(ruta);
+            if(file.exists()) {
+                file.delete();
+            }
+            file.getParentFile().mkdirs();
         }
-
-        return null;
     }
 }
---
+++ Pr2/src/model/main.java
@@ -0,0 +1,10 @@
+package model;
+
+import controller.Controller;
+
+public class main {
+    //Main
+    public static void main(String[] args) {
+        Controller.menu();
+    }
+}
